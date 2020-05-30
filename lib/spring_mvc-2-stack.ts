22d780466b47b2e9eedb887d8f6a9a1322b261c6
Wenjing <he.wenjing99@gmail.com>
2020-05-30 15:50:29 -0700

    Add autoscaling and use online build dokcer image

--- lib/spring_mvc-2-stack.ts
+++ lib/spring_mvc-2-stack.ts
@@ -8,8 +8,8 @@ export class SpringMvc2Stack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "SpringVpc", {
-      maxAzs: 3
-      // natGateways: 1 // Default is all AZs in region
+      maxAzs: 3,
+      natGateways: 1
     });
 
     const cluster = new ecs.Cluster(this, "SpringCluster", {
@@ -18,13 +18,35 @@ export class SpringMvc2Stack extends cdk.Stack {
     });
 
     // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+    const springbootApp = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
       cluster: cluster, // Required
       cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      desiredCount: 1, // Default is 1
+      taskImageOptions: {
+        containerPort: 8080,
+        image: ecs.ContainerImage.fromAsset('./sb-app')
+      },
       memoryLimitMiB: 2048, // Default is 512
       publicLoadBalancer: true // Default is false
     });
+    springbootApp.targetGroup.configureHealthCheck({
+      "port": 'traffic-port',
+          "path": '/actuator/health',
+          "interval": cdk.Duration.seconds(5),
+          "timeout": cdk.Duration.seconds(4),
+          "healthyThresholdCount": 2,
+          "unhealthyThresholdCount": 2,
+          "healthyHttpCodes": "200,301,302"
+    });
+    const springbootAutoScaling = springbootApp.service.autoScaleTaskCount({
+      maxCapacity: 6,
+      minCapacity: 1
+  });
+  springbootAutoScaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 45,
+      policyName: "cpu autoscaling",
+      scaleInCooldown: cdk.Duration.seconds(30),
+      scaleOutCooldown: cdk.Duration.seconds(30)
+  });
   }
 }
